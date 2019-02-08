c2fc4525efa938456325689aebab82d201750383
derGoldeneJonas <jonas@bender-home.eu>
2019-02-08 18:07:05 +0100

    changes speed-dreams init and create page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -12,7 +12,7 @@ let flag = 0;
     styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+    public data = 'test';
     constructor() {
     }
 
@@ -22,35 +22,60 @@ export class HomeComponent implements OnInit {
     test() {
         const nodePath = (shell.which('node').toString());
         shell.config.execPath = nodePath;
-        let command = shell.exec('/home/tobias/speed-dreams/build/games/speed-dreams-2 --text-only', {silent: false, async: true});
+        let command = shell.exec('/home/user1/speed-dreams/build/games/speed-dreams-2 -s quickrace -x', {silent: false, async: true});
         command.stdout.on('data', (data) => {
         });
 
+        shell.cd('/home/user1/operating-system/');
+        let command2 = shell.exec('make vde < pass.txt', {silent: false, async: true});
+
+        let command3 = shell.exec('PROJECT=idp_acc make jenkins_run', {silent: false, async: true});
+        command3.stdout.on('data', function (data) {
+            if (data.includes('mosquitto server')) {
+                //if(flag === 0) {
+                //flag = 1;
+                    let command4 = shell.exec('PROJECT=idp_savm make jenkins_run', {silent: false, async: true});
+                    // command4.stdout.on('data', function (data1) {
+                    //     if (data1.includes('connected to mosquitto server')) {
+                    //         let command6 = shell.exec('mosquitto_sub -t "#" -v', {silent: false, async: true});
+                    //         //data = data1;
+                    //     }
+                    // });
+                setTimeout(function () {
+                    let command10 = shell.exec('pkill -P $$', {silent: false, async: true});
+                  command.kill();
+                }, 19000);
+            }
+        });
     }
 
     test1() {
-        shell.cd('/home/tobias/operating-system/');
-        let command2 = shell.exec('make vde', {silent: false, async: true});
-        setTimeout(function () {
+        shell.cd('/home/user1/operating-system/');
+        let command2 = shell.exec('make vde < pass.txt', {silent: false, async: true});
+       // let command8 = shell.exec('echo administrator', {silent: false, async: true});
+
+       /* setTimeout(function () {
             let command5 = shell.pushd('hallo123');
-        }, 5000,);
+        }, 5000,);*/
     }
 
     test2() {
-        shell.cd('/home/tobias/operating-system/');
+        shell.cd('/home/user1/operating-system/');
         //let command2 = shell.exec('make vde', {silent: false, async: true});
         let command3 = shell.exec('PROJECT=idp_acc make jenkins_run', {silent: false, async: true});
         command3.stdout.on('data', function (data) {
-            if (data.includes('connected to mosquitto server')) {
+            if (data.includes('mosquitto server')) {
                 //if(flag === 0) {
                 //flag = 1;
-                let command4 = shell.exec('PROJECT=idp_savm make jenkins_run', {silent: false, async: true});
-                command4.stdout.on('data', function (data) {
-                    if (data.includes('Go')) {
-                        let command6 = shell.exec('mosquitto_sub -t "#" -v', {silent: false, async: true});
-                    }
-                });
-                // }
+                setTimeout(function () {
+                    let command4 = shell.exec('PROJECT=idp_savm make jenkins_run', {silent: false, async: true});
+                    command4.stdout.on('data', function (data1) {
+                        if (data1.includes('connected to mosquitto server')) {
+                            let command6 = shell.exec('mosquitto_sub -t "#" -v', {silent: false, async: true});
+                            //data = data1;
+                        }
+                    });
+                }, 4000);
             }
         });
         //setTimeout(function(){
