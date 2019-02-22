52ff1018393c7a556b5777c7ea13efa4d4fdc642
Carmen <carmen@Carmens-MacBook-Air.local>
2019-02-22 12:52:27 +0100

    newcomponents

---
+++ src/app/components/resultoverview/resultoverview.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+//import { DataService } from '../../data.service';
+@Component({
+  selector: 'app-resultoverview',
+  templateUrl: './resultoverview.component.html',
+  styleUrls: ['./resultoverview.component.scss']
+})
+export class ResultoverviewComponent implements OnInit {
+
+    users$: Object;
+
+    constructor(private data: DataService) { }
+
+    ngOnInit() {
+        this.data.getResultData().subscribe(
+            data => this.users$ = data
+        );
+    }
+
+}
