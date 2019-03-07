548859e1d378aceb57957c28e6ac35394f2ea6fd
Carmen <carmen@carmens-air.speedport.ip>
2019-03-07 14:02:32 +0100

    styling liste

--- src/app/components/resultoverview/resultoverview.component.ts
+++ src/app/components/resultoverview/resultoverview.component.ts
@@ -11,23 +11,23 @@ import {testsetresult} from '../../models/testsetresult.js';
   styleUrls: ['./resultoverview.component.scss']
 })
 export class ResultoverviewComponent implements OnInit {
-    title = 'Data binding using String Interpolation';
-    // test = this.testCarmen2();
-    users$: Object;
     public displayedColumns = ['name', 'id', 'duration', 'Testset_id'];
     dataSource = new MatTableDataSource<testsetresult>();
-    tabs = ['First', 'Second', 'Third'];
-    selected = new FormControl(0);
 
-    constructor(private dataService: DataService) { }
+    constructor(private dataService: DataService) {
+    }
 
     ngOnInit() {
         this.dataService.readAllTestsetResult().subscribe(
-            data => {this.dataSource.data = data as testsetresult[]; console.log(data); }
+            data => {
+                this.dataSource.data = data as testsetresult[];
+                console.log(data);
+            }
         );
     }
+}
 
-    testCarmen1() {
+/*    testCarmen1() {
         const carmen1 = this.dataService.readAllTestsetResult();
         console.log('did the first thing');
         // console.log(carmen1[0].name);
@@ -45,28 +45,7 @@ export class ResultoverviewComponent implements OnInit {
         // this.dataService.readAllTestsetResult().subscribe({
          //   next: function() {}}
          // );
-    }
-
-    addTab(selectAfterAdding: boolean) {
-        this.tabs.push('New');
-
-        if (selectAfterAdding) {
-            this.selected.setValue(this.tabs.length - 1);
-        }
-    }
-
-    removeTab(index: number) {
-        this.tabs.splice(index, 1);
-    }
-
-    startTest() {
-        console.log('test');
-    }
-    /*navigateTo(item, event) {
-      this.activeItem = item;
     }*/
-}
-
 
 
 /* users$: Object;
@@ -83,22 +62,3 @@ export class ResultoverviewComponent implements OnInit {
 
 /* LIST CODE */
 
-export interface PeriodicElement {
-    name: string;
-    position: number;
-    weight: number;
-    symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-    {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-    {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-    {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-    {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-    {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-    {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-    {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-    {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-    {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-    {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
