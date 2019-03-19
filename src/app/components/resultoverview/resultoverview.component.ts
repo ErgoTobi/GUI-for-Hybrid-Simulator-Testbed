72fa8a29dae7f9135c49c25cb956bf8fd856d5d9
ErgoTobi <bartsch.tobias@gmx.de>
2019-03-19 18:29:32 +0100

    Data Service incl. neue Datenbank Struktur added & funktioniert ResultOverview reintroduced - Teil2 TB

--- src/app/components/resultoverview/resultoverview.component.ts
+++ src/app/components/resultoverview/resultoverview.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {DataService} from '../../data.service';
 
 
@@ -9,20 +9,19 @@ import {DataService} from '../../data.service';
 })
 export class ResultoverviewComponent implements OnInit {
     title = 'Data binding using String Interpolation';
-    @ViewChild('lastNameInput') nameInputRef: ElementRef;
     // test = this.testCarmen2();
     users$: Object;
 
     constructor(private dataService: DataService) { }
 
     ngOnInit() {
-        this.dataService.readAllTestsetResultsOnly().subscribe(
+        this.dataService.readAllTestsetResult().subscribe(
             data => {this.users$ = data; console.log(data); }
         );
     }
 
     testCarmen1() {
-        const carmen1 = this.dataService.readAllTestsetResultsOnly();
+        const carmen1 = this.dataService.readAllTestsetResult();
         console.log('did the first thing');
         // console.log(carmen1[0].name);
         // const carmen1_1 = carmen1[1].get('name');
@@ -30,10 +29,10 @@ export class ResultoverviewComponent implements OnInit {
         // console.log(carmen1_1);
     }
     testCarmen2() {
-        const carmen2 = this.dataService.readAllTestsetResultsOnly();
+        const carmen2 = this.dataService.readTestsetresult(2);
         console.log('show id');
         console.log(carmen2);
-        this.dataService.readAllTestsetResultsOnly().subscribe(
+        this.dataService.readAllTestsetResult().subscribe(
             data => {this.users$ = data; console.log(data); }
         );
         // this.dataService.readAllTestsetResult().subscribe({
--- src/app/models/Testset.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-module.exports = (sequelize, DataTypes) => {
-  const Testset = sequelize.define('Testset', {
-    name: DataTypes.STRING
-  }, {});
-  Testset.associate = function(models) {
-  };
-  return Testset;
-};
