cd7797527c2096ccca9144099913ff440ade2f54
derGoldeneJonas <jonas@bender-home.eu>
2019-03-21 12:15:20 +0100

    changes speed-dreams init and create page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -19,7 +19,10 @@ const mqtt = require('mqtt');
 export class HomeComponent implements OnInit {
    // public data = 'test';
     @ViewChild('lastNameInput') nameInputRef: ElementRef;
-    constructor(private dataService: DataService) { }
+    activeChunk: string;
+
+    constructor(private dataService: DataService) {
+    }
 
     ngOnInit() {
     }
@@ -27,12 +30,12 @@ export class HomeComponent implements OnInit {
     test() {
         const nodePath = (shell.which('node').toString());
         shell.config.execPath = nodePath;
-        // let command = shell.exec('/home/user1/speed-dreams/build/games/speed-dreams-2 -s quickrace', {silent: false, async: true});
+        let command = shell.exec('/home/user1/speed-dreams/build/games/speed-dreams-2 -s quickrace', {silent: false, async: true});
         // command.stdout.on('data', (data) => {
         //  });
 
         shell.cd('/home/user1/operating-system/');
-        // let command2 = shell.exec('echo administrator |--stdin make vde', {silent: false, async: true});
+        let command2 = shell.exec('echo administrator | sudo -S make vde', {silent: false, async: true});
         //let command23 = shell.exec('make vde', {silent: false, async: true});
 
         let command3 = shell.exec('PROJECT=idp_acc make jenkins_run', {silent: false, async: true});
@@ -137,23 +140,66 @@ export class HomeComponent implements OnInit {
         this.dataService.createTestsetResult('WSTestsetResult', Sequelize.fn('NOW'), Sequelize.fn('NOW'), 2);
         this.dataService.createScenarioResult('WSTestsetResult', Sequelize.fn('NOW'), Sequelize.fn('NOW'), 3, 2);
         this.dataService.createRunResult(Sequelize.fn('NOW'), Sequelize.fn('NOW'), 'failed', 3);
-        this.dataService.createRunDetail (Sequelize.fn('NOW'), 4.66, 8.98, 2,
-            'WSAudi', true, false, 4);
+        this.dataService.createRunDetail (Sequelize.fn('NOW'), Sequelize.fn('NOW'), 0, false, false,
+            352.707672, 19.758799, 345.900513, 13.071578, 348.513123,
+            13.969010, 338.821075, 13.845463, 334.953339,
+            12.287340, 332.261292, 12.163793, 8.417906, 6.142469,
+            2, 0.485398, 203.593704, 892.117920, 0.081214, 0.000044,
+            0.000034, 	0.000056, 0.000078, 'WSacc', 0.034794, 2.392970, 0.000023,
+            0.000023, 	0.000034, 	0.000045,	2, 1);
     }
 
     test5() {
+        const service = this.dataService;
         const client = mqtt.connect([{host: 'localhost', port: 1883}]);
         client.on('connect', function () {
+            client.subscribe('savm/car/0/isPositionTracked', function (err) {
+                if (err) {
+                    client.publish('savm/car/0/isPositionTracked', 'Error: Missing Data');
+                }
+            });
+            client.subscribe('savm/car/0/isSpeedTracked', function (err) {
+                if (err) {
+                    client.publish('savm/car/0/isSpeedTracked', 'Error: Missing Data');
+                }
+            });
+            client.subscribe('savm/car/0/leadPos', function (err) {
+                if (err) {
+                    client.publish('savm/car/0/leadPos', 'Error: Missing Data');
+                }
+            });
             client.subscribe('savm/car/0/ownPos', function (err) {
-                if (!err) {
-                    client.publish('ecu', 'error');
+                if (err) {
+                    client.publish('savm/car/0/ownPos', 'Error: Missing Data');
+                }
+            });
+            client.subscribe('savm/car/0/leadSpeed', function (err) {
+                if (err) {
+                    client.publish('savm/car/0/leadSpeed', 'Error: Missing Data');
+                }
+            });
+            client.subscribe('savm/car/0/ownSpeed', function (err) {
+                if (err) {
+                    client.publish('savm/car/0/ownSpeed', 'Error: Missing Data');
+                }
+            });
+            client.subscribe('savm/car/0/curGear', function (err) {
+                if (err) {
+                    client.publish('savm/car/0/curGear', 'Error: Missing Data');
                 }
             });
         });
 
-        client.on('message', function (topic, message, packet) {
+        client.on('message', function (topic, message) {
             // message is Buffer
-            console.log(message);
+            sessionStorage.setItem(topic, message.toString());
+            if (topic === 'savm/car/0/curGear') {
+                service.createRunDetail(sessionStorage.getItem('savm/car/0/isPositionTracked'),
+                    sessionStorage.getItem('savm/car/0/isPositionTracked'), sessionStorage.getItem('savm/car/0/isPositionTracked'),
+                    sessionStorage.getItem('savm/car/0/isPositionTracked'), sessionStorage.getItem('savm/car/0/isPositionTracked'),
+                    sessionStorage.getItem('savm/car/0/isPositionTracked'),
+                    sessionStorage.getItem('savm/car/0/isPositionTracked'), sessionStorage.getItem('savm/car/0/isPositionTracked'));
+            }
         });
         // this.dataService.updateSuite('Tobi', 'Hi', true);
     }
